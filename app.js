10f2143aa344e2178699840eeef564cf6fbfeced
Sin3qz <patrickdietrich2@gmail.com>
2026-05-14 14:44:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 async function load() {
+
   const res = await fetch("signals.json");
   const data = await res.json();
 
   const table = document.getElementById("table");
+
   table.innerHTML = "";
 
-  data.forEach(d => {
+  data.table.forEach(d => {
+
     table.innerHTML += `
       <tr>
+        <td>${d.rank}</td>
         <td>${d.name}</td>
         <td>${d.price.toFixed(2)}</td>
-        <td>${(d.momentum*100).toFixed(2)}%</td>
-        <td>${d.sma150.toFixed(2)}</td>
+        <td>${(d.momentum * 100).toFixed(2)}%</td>
+        <td>${d.price > d.sma150 ? "✅" : "❌"}</td>
         <td>${d.smaCheck ? "✅" : "❌"}</td>
         <td>${d.valid ? "VALID" : "OUT"}</td>
-        <td>${d.rank}</td>
       </tr>
     `;
   });
+
 }
 
 load();
