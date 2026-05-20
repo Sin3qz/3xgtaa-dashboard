7c364f85bac9011e1106bb8afa40c439d50b930e
Sin3qz <patrickdietrich2@gmail.com>
2026-05-20 15:52:01 +0200

    Create app1x.js

---
+++ app1x.js
@@ -0,0 +1,98 @@
+async function load() {
+  const res = await fetch("signals1x.json");
+  const data = await res.json();
+
+  const table = document.getElementById("table");
+  const prices = document.getElementById("prices");
+  const tipsBox = document.getElementById("tipsBox");
+
+  table.innerHTML = "";
+  prices.innerHTML = "";
+  tipsBox.innerHTML = "";
+
+  if (data.tips) {
+    tipsBox.innerHTML += `
+      <div class="tips-title">TIPS</div>
+      <div class="${data.tips.sma200Pct >= 0 ? "green" : "red"}">
+        ${data.tips.sma200Pct >= 0 ? "+" : ""}${data.tips.sma200Pct.toFixed(2)}%
+        ${data.tips.sma200Pct >= 0 ? "über SMA200" : "unter SMA200"}
+      </div>
+    `;
+  }
+
+  if (data.spy) {
+    tipsBox.innerHTML += `
+      <br>
+      <div class="tips-title">SPY</div>
+      <div class="${data.spy.sma150Pct >= 0 ? "green" : "red"}">
+        ${data.spy.sma150Pct >= 0 ? "+" : ""}${data.spy.sma150Pct.toFixed(2)}%
+        ${data.spy.sma150Pct >= 0 ? "über SMA150" : "unter SMA150"}
+      </div>
+    `;
+  }
+
+  data.table.forEach(d => {
+    let rowClass = "";
+
+    if (d.slot === 1) {
+      rowClass = "invested-one";
+    }
+
+    if (d.slot === 2) {
+      rowClass = "invested-two";
+    }
+
+    table.innerHTML += `
+      <tr class="${rowClass}">
+        <td>${d.rank}</td>
+        <td>${d.name}</td>
+        <td class="${d.m1 >= 0 ? 'green' : 'red'}">${d.m1.toFixed(2)}</td>
+        <td class="${d.m3 >= 0 ? 'green' : 'red'}">${d.m3.toFixed(2)}</td>
+        <td class="${d.m6 >= 0 ? 'green' : 'red'}">${d.m6.toFixed(2)}</td>
+        <td class="${d.m9 >= 0 ? 'green' : 'red'}">${d.m9.toFixed(2)}</td>
+        <td class="${d.momentum >= 0 ? 'green' : 'red'}">${d.momentum.toFixed(2)}</td>
+        <td class="${d.sma150Pct >= 0 ? 'green' : 'red'}">${d.sma150Pct.toFixed(2)}</td>
+        <td class="${d.sma20Pct >= 0 ? 'green' : 'red'}">${d.sma20Pct.toFixed(2)}</td>
+      </tr>
+    `;
+
+    prices.innerHTML += `
+      <tr>
+        <td>${d.name}</td>
+        <td>${d.p1m.toFixed(2)}</td>
+        <td>${d.p3m.toFixed(2)}</td>
+        <td>${d.p6m.toFixed(2)}</td>
+        <td>${d.p9m.toFixed(2)}</td>
+        <td>${d.current.toFixed(2)}</td>
+      </tr>
+    `;
+  });
+
+  if (data.tips) {
+    prices.innerHTML += `
+      <tr>
+        <td>TIPS</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>SMA200: ${data.tips.sma200.toFixed(2)}</td>
+        <td>${data.tips.current.toFixed(2)}</td>
+      </tr>
+    `;
+  }
+
+  if (data.spy) {
+    prices.innerHTML += `
+      <tr>
+        <td>SPY</td>
+        <td>-</td>
+        <td>-</td>
+        <td>-</td>
+        <td>SMA150: ${data.spy.sma150.toFixed(2)}</td>
+        <td>${data.spy.current.toFixed(2)}</td>
+      </tr>
+    `;
+  }
+}
+
+load();
