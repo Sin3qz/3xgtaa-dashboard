4882f9f9370a01ead4346d99425353df84914556
Sin3qz <patrickdietrich2@gmail.com>
2026-05-26 16:16:52 +0200

    Update strategy.js

--- strategy.js
+++ strategy.js
@@ -122,14 +122,14 @@ function buildInvestments(results, previousPairType) {
 }
 
 async function getTipsData() {
-  const prices = await fetchData("ITPS.DE");
+  const prices = await fetchData("TI5G.L");
 
   const current = prices.at(-1);
   const sma200 = sma(prices, 200);
   const sma200Pct = ((current / sma200) - 1) * 100;
 
   return {
-    symbol: "ITPS.DE",
+    symbol: "TI5G.L",
     current,
     sma200,
     sma200Pct
