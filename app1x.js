f90fc58f39957b9cad7cb0c9a7fe1a297ee5867d
Sin3qz <patrickdietrich2@gmail.com>
2026-05-28 11:45:17 +0200

    Update app1x.js

--- app1x.js
+++ app1x.js
@@ -1,302 +1,98 @@
-async function load() {
+async function loadSignals() {
 
-  const res =
+  const response =
     await fetch("signals1x.json");
 
   const data =
-    await res.json();
+    await response.json();
 
-  const table =
-    document.getElementById("table");
+  const tbody =
+    document.querySelector("#rankingTable tbody");
 
-  const prices =
-    document.getElementById("prices");
+  tbody.innerHTML = "";
 
-  const tipsBox =
-    document.getElementById("tipsBox");
+  data.results.forEach(asset => {
 
-  table.innerHTML = "";
-  prices.innerHTML = "";
-  tipsBox.innerHTML = "";
+    const tr =
+      document.createElement("tr");
 
-  // TIPS
-  if (data.tips) {
-
-    tipsBox.innerHTML += `
-
-      <div class="tips-title">
-        TIPS (EUR hedged)
-      </div>
-
-      <div class="${
-        data.tips.sma200Pct >= 0
-          ? "green"
-          : "red"
-      }">
-
-        ${
-          data.tips.sma200Pct >= 0
-            ? "+"
-            : ""
-        }
-
-        ${data.tips.sma200Pct.toFixed(2)}%
-
-        ${
-          data.tips.sma200Pct >= 0
-            ? "über SMA200"
-            : "unter SMA200"
-        }
-
-      </div>
-    `;
-  }
-
-  // SP500 EUR
-  if (data.spy) {
-
-    tipsBox.innerHTML += `
-
-      <br>
-
-      <div class="tips-title">
-        S&P500 (EUR hedged)
-      </div>
-
-      <div class="${
-        data.spy.sma150Pct >= 0
-          ? "green"
-          : "red"
-      }">
-
-        ${
-          data.spy.sma150Pct >= 0
-            ? "+"
-            : ""
-        }
-
-        ${data.spy.sma150Pct.toFixed(2)}%
-
-        ${
-          data.spy.sma150Pct >= 0
-            ? "über SMA150"
-            : "unter SMA150"
-        }
-
-      </div>
-    `;
-  }
-
-  // GOLD
-  if (data.goldMacro) {
-
-    tipsBox.innerHTML += `
-
-      <br>
-
-      <div class="tips-title">
-        Gold (EUR)
-      </div>
-
-      <div class="${
-        data.goldMacro.sma150Pct >= 0
-          ? "green"
-          : "red"
-      }">
-
-        ${
-          data.goldMacro.sma150Pct >= 0
-            ? "+"
-            : ""
-        }
-
-        ${data.goldMacro.sma150Pct.toFixed(2)}%
-
-        ${
-          data.goldMacro.sma150Pct >= 0
-            ? "über SMA150"
-            : "unter SMA150"
-        }
-
-      </div>
-    `;
-  }
-
-  data.table.forEach(d => {
-
-    let rowClass = "";
-
-    if (d.slot === 1) {
-      rowClass = "invested-one";
+    if (asset.slot === 1) {
+      tr.classList.add("top1");
     }
 
-    if (d.slot === 2) {
-      rowClass = "invested-two";
+    if (asset.slot === 2) {
+      tr.classList.add("top2");
     }
 
-    table.innerHTML += `
-
-      <tr class="${rowClass}">
-
-        <td>${d.rank}</td>
-
-        <td>${d.name}</td>
-
-        <td class="${
-          d.m1 >= 0
-            ? "green"
-            : "red"
-        }">
-          ${d.m1.toFixed(2)}
-        </td>
-
-        <td class="${
-          d.m3 >= 0
-            ? "green"
-            : "red"
-        }">
-          ${d.m3.toFixed(2)}
-        </td>
-
-        <td class="${
-          d.m6 >= 0
-            ? "green"
-            : "red"
-        }">
-          ${d.m6.toFixed(2)}
-        </td>
-
-        <td class="${
-          d.m9 >= 0
-            ? "green"
-            : "red"
-        }">
-          ${d.m9.toFixed(2)}
-        </td>
-
-        <td class="${
-          d.momentum >= 0
-            ? "green"
-            : "red"
-        }">
-          ${d.momentum.toFixed(2)}
-        </td>
-
-        <td class="${
-          d.sma150Pct >= 0
-            ? "green"
-            : "red"
-        }">
-          ${d.sma150Pct.toFixed(2)}
-        </td>
-
-        <td class="${
-          d.sma20Pct >= 0
-            ? "green"
-            : "red"
-        }">
-          ${d.sma20Pct.toFixed(2)}
-        </td>
-
-      </tr>
+    tr.innerHTML = `
+      <td>${asset.rank}</td>
+      <td>${asset.name}</td>
+      <td>${asset.momentum.toFixed(2)}</td>
+      <td>${asset.sma150Pct.toFixed(2)}</td>
+      <td>${asset.sma20Pct.toFixed(2)}</td>
     `;
 
-    prices.innerHTML += `
+    tbody.appendChild(tr);
 
-      <tr>
-
-        <td>${d.name}</td>
-
-        <td>${d.p1m.toFixed(2)}</td>
-
-        <td>${d.p3m.toFixed(2)}</td>
-
-        <td>${d.p6m.toFixed(2)}</td>
-
-        <td>${d.p9m.toFixed(2)}</td>
-
-        <td>${d.current.toFixed(2)}</td>
-
-      </tr>
-    `;
   });
 
-  // Makro-Tabelle
+  const historyBody =
+    document.querySelector("#historyTable tbody");
 
-  if (data.tips) {
+  historyBody.innerHTML = "";
 
-    prices.innerHTML += `
+  data.history.forEach(h => {
 
-      <tr>
+    const tr =
+      document.createElement("tr");
 
-        <td>TIPS (EUR hedged)</td>
-
-        <td>-</td>
-        <td>-</td>
-        <td>-</td>
-
-        <td>
-          SMA200:
-          ${data.tips.sma200.toFixed(2)}
-        </td>
-
-        <td>
-          ${data.tips.current.toFixed(2)}
-        </td>
-
-      </tr>
+    tr.innerHTML = `
+      <td>${h.date}</td>
+      <td>${h.invested.join(", ")}</td>
     `;
-  }
 
-  if (data.spy) {
+    historyBody.appendChild(tr);
 
-    prices.innerHTML += `
+  });
 
-      <tr>
-
-        <td>S&P500 (EUR)</td>
-
-        <td>-</td>
-        <td>-</td>
-        <td>-</td>
-
-        <td>
-          SMA150:
-          ${data.spy.sma150.toFixed(2)}
-        </td>
-
-        <td>
-          ${data.spy.current.toFixed(2)}
-        </td>
-
-      </tr>
+  document.getElementById("tipsBox").innerHTML =
+    `
+      <strong>TIPS</strong><br>
+      ${data.tips.sma200Pct >= 0 ? "+" : ""}
+      ${data.tips.sma200Pct.toFixed(2)}%
     `;
-  }
 
-  if (data.goldMacro) {
-
-    prices.innerHTML += `
-
-      <tr>
-
-        <td>Gold (EUR)</td>
-
-        <td>-</td>
-        <td>-</td>
-        <td>-</td>
-
-        <td>
-          SMA150:
-          ${data.goldMacro.sma150.toFixed(2)}
-        </td>
-
-        <td>
-          ${data.goldMacro.current.toFixed(2)}
-        </td>
-
-      </tr>
+  document.getElementById("spyBox").innerHTML =
+    `
+      <strong>S&P500 (EUR hedged)</strong><br>
+      ${data.spy.sma150Pct >= 0 ? "+" : ""}
+      ${data.spy.sma150Pct.toFixed(2)}%
     `;
+
+  document.getElementById("goldBox").innerHTML =
+    `
+      <strong>Gold</strong><br>
+      ${data.gold.sma150Pct >= 0 ? "+" : ""}
+      ${data.gold.sma150Pct.toFixed(2)}%
+    `;
+
+  const lastUpdate =
+    document.getElementById("lastUpdate");
+
+  if (data.updated && lastUpdate) {
+
+    const date =
+      new Date(data.updated);
+
+    lastUpdate.innerHTML =
+      "Letzte erfolgreiche Signalaktualisierung: " +
+      date.toLocaleString("de-DE", {
+        timeZone: "Europe/Berlin",
+        dateStyle: "medium",
+        timeStyle: "short"
+      }) +
+      " Uhr";
   }
 }
 
-load();
+loadSignals();
