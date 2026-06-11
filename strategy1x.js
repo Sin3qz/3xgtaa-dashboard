f25f657ef4bbc0c28b66b70cc3368f05782dc35e
Sin3qz <patrickdietrich2@gmail.com>
2026-06-11 17:41:44 +0200

    Update strategy1x.js

--- strategy1x.js
+++ strategy1x.js
@@ -105,7 +105,13 @@ function expectedFreshDateForAsset(asset) {
   const yesterday =
     yesterdayBerlinString();
 
-  if (asset.type === "BTC") {
+   if (
+    asset.type === "BTC" ||
+    asset.type === "USDLONG" ||
+    asset.type === "USDSHORT" ||
+    asset.symbol === "USDEUR=X" ||
+    asset.symbol === "EURUSD=X"
+  ) {
     return yesterday;
   }
 
